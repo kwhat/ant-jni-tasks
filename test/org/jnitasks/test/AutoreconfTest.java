bbf0a95a59dba98eae8bac31429c1b2ff83e8c64
Alexander Barker <alex@1stleg.com>
2014-08-10 21:30:00 -0700

    Cleaned up all (c) headers.

--- test/org/jnitasks/test/AutoreconfTest.java
+++ test/org/jnitasks/test/AutoreconfTest.java
@@ -1,12 +1,30 @@
+/* JNITasks: Ant tasks for JNI projects.
+ * Copyright (C) 2013-2014 Alexander Barker.  All Rights Received.
+ * https://github.com/kwhat/jnitasks/
+ *
+ * JNITasks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libUIOHook is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.jnitasks.test;
 
-import java.io.File;
-import java.io.PrintWriter;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.BuildFileTest;
 import org.apache.tools.ant.Project;
 import org.jnitasks.autotools.AutoreconfTask;
 import org.jnitasks.test.autotools.VerboseLogger;
+
+import java.io.File;
+import java.io.PrintWriter;
 
 public class AutoreconfTest extends BuildFileTest {
 	private VerboseLogger logger;
